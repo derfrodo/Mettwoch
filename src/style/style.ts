c1c1e1608ed188171b6c1f93c33661eeaff65867
stefan <stefan@derfrodo.de>
2018-01-14 18:52:01 +0100

    styles...

--- src/style/style.ts
+++ src/style/style.ts
@@ -1,3 +1,9 @@
-import "./../Order/style/style.css";
-
+// vendors
+import "./../../node_modules/bootstrap/dist/css/bootstrap.css";
 import "rc-slider/assets/index.css";
+
+// general styles
+import "./styles.css";
+
+// feature specific styles
+import "./../Order/style/style.css";
